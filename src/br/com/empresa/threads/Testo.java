bc6db52a6a7943d21105116f0d2f61a247b79036
rogeriop <pinheiroxrogerio@gmail.com>
2016-04-12 08:54:29 -0300

    fim 06 - Threads

---
+++ src/br/com/empresa/threads/Testo.java
@@ -0,0 +1,19 @@
+package br.com.empresa.threads;
+
+public class Testo {
+	public static void main(String[] args) {
+		
+	        int a = args.length;
+	        int i = 0;
+	        switch(a) {
+	            case 0:
+	            case 1:
+	            for(i=0;i<15;i++, System.out.println(i))
+	            if(i==5) continue;
+	            if(i==15) break;
+	            case 2:
+	            System.out.println("2");
+	        }
+	        System.out.println("fim");
+	    }	}
+
